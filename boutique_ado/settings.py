d4790c2bf4a68a376ee67e3f224a5fc9d310b0bd
ciaralynch72 <Ciaralynch72@gmail.com>
2023-04-01 23:05:02 +0000

    Add static info

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -167,15 +167,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATICFILES_ROOT = (os.path.join(BASE_DIR, 'static'),)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-ciara'
-    AWS_S3_REGION_NAME = 'us-east-2'
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-cl2'
+    AWS_S3_REGION_NAME = 'ca-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
